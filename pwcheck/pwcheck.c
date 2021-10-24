a6a741c23e0ae5b1d1117d0a2d026cae8851097a
OndraPotocek <56274452+OndraPotocek@users.noreply.github.com>
2021-10-24 18:07:39 +0200

    Add files via upload

--- pwcheck/pwcheck.c
+++ pwcheck/pwcheck.c
@@ -1,35 +1,10 @@
 #include <stdio.h>
 
-# define LEVEL 4
-# define PARAM 3
+int main(int argc, char const *argv[]){
+    char pw[20];
+    fgets(pw, 20, stdin);
 
-int containsLowAndUpperCharacter(char pw[]){
-    for(int i=0;i!='\0';i++){
-        
-    }
-    return 1; 
-}
-
-int main(){
-        
-    const char passwords[] = "heslo\ndelsiheslo\nhesloscislemtreba123\nhesloscislem5aspecialnimznakem;";
-    char pw[100];
-    int pointer = 0;
-
-    for(int counter = 0; counter< sizeof passwords/ sizeof passwords[0]; counter++){
-        if (passwords[counter]!='\n' || passwords[counter]!='\0'){
-            pw[pointer] += passwords[counter];
-            pointer+=1;
-        }
-
-        else{
-            printf("%s", pw);
-            pointer = 0;
-        }
-        
-    }
-    
-    
+    printf("%s", pw);
     
     return 0;
 }
