d2fcee2a15f74dcfd1f200109e63ab579b553ec3
OndraPotocek <56274452+OndraPotocek@users.noreply.github.com>
2021-10-31 16:54:51 +0100

    Add files via upload

--- pwcheck/pwcheck.c
+++ pwcheck/pwcheck.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+
 #define COUNT_OF_ASCII 100
 
 
 
-int containsLowAndUpperCharacter(char word[]){
+bool containsLowAndUpperCharacter(char word[]){
     bool lowCharacer=false;
     bool upperCharacter = false;
-    for(int i = 0;i<sizeof(word)/sizeof(word[0]);i++){
+    for(int i = 0;i<(sizeof(word)/sizeof(word[0]))+COUNT_OF_ASCII;i++){
         if(word[i] >= 'a' && word[i] <='z'){
             lowCharacer=true;
         }
@@ -17,49 +18,53 @@ int containsLowAndUpperCharacter(char word[]){
         }
     } 
     if(lowCharacer && upperCharacter){
-        return 1;
+        return true;
     }   
     else{
-        return 0;
+        return false;
     }
 }
 
-int havingRightCharset(char word[], int param){
-    int checker = 0;
-    bool lowNO=true,upNO=true,numNO=true,specNO=true;
-    for(int i = 0;i<sizeof(word)/sizeof(word[0]);i++){
-        if(word[i] >= 'a' && word[i] <='z' && lowNO){
-            checker+=1;
-            lowNO=false;
+bool havingRightCharset(char word[], int param){
+    int low = 0, up = 0, num = 0, spec = 0;
+    bool foundLow = false, foundUp = false, foundNum = false, foundSpec = false;
+    for(int i = 0;i<(sizeof(word)/sizeof(word[0]))+COUNT_OF_ASCII;i++){
+        if(word[i] == '\0'){
+            break;
         }
-        if(word[i] >= 'A' && word[i] <='Z' && upNO){
-            checker+=1;
-            upNO=false;
+        if(word[i] >= 'a' && word[i] <='z'){
+            low = 1;
+            
         }
-        if(word[i] >= '0' && word[i] <='9' && numNO){
-            checker+=1;
-            numNO=false;
+        if(word[i] >= 'A' && word[i] <='Z'){
+            up = 1;
+            
         }
-        if((word[i] >= 32 && word[i] <= 47) || (word[i] >= 58 && word[i] <= 64) || (word[i] >= 91 && word[i] <= 96) || (word[i] >= 123 && word[i] <= 126) && specNO){
-            checker+=1;
-            specNO=false;
+        if(word[i] >= '0' && word[i] <='9'){
+            num = 1;
+            
         }
-    }
-    if(param == 1 && checker>=1){
-        return 1;
-    }
-    if(param == 2 && checker>=2){
-        return 1;
-    }
-    if(param == 3 && checker>=3){
-        return 1;
-    }
-    if(param == 4 && checker>=4){
-        return 1;
+        if((word[i] >= 32 && word[i] <= 47) || (word[i] >= 58 && word[i] <= 64) || (word[i] >= 91 && word[i] <= 96) || (word[i] >= 123 && word[i] <= 126)){
+            spec = 1;
+            
+        }
+        if(param == 1 && low + up + num + spec >= 1){
+            return true;
+        }
+        if(param == 2 && low + up + num + spec >= 2){
+            return true;
+        }
+        if(param == 3 && low + up + num + spec >= 3){
+            return true;
+        }
+        if(param == 4 && low + up + num + spec >= 4){
+            return true;
+        }   
     }
     
     
-    return 0;
+    return false;
+    
 } 
 
 bool notContainsSameChars(char word[], int param){
@@ -139,92 +144,157 @@ int maximal(char pw[], int previousLength){
 }
 
 
-bool notContainsSameStrings(char pw[], int delka)
-{
-    bool stejnePodretezce = true;
-    switch (delka)
-    {
-        case 1: stejnePodretezce = false;
-        case 2:
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    if (pw[i] == '\0')break;
-                    for (int j = (1 + i); j < 100; j++)
-                    {
-                        if (pw[i] == pw[j])
-                        {
-                            if (pw[i+1] == pw[j+1]) stejnePodretezce = false;
-                        }
-                    }
-                }
+bool notContainsSameStrings(char word[], int param){
+    bool notsamestrings = true;
+        
+    
+    if(param == 2){
+        for (int i = 0; i < COUNT_OF_ASCII; i++){
+            if (word[i] == '\0'){
                 break;
             }
-        case 3:
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    if (pw[i] == '\0')break;
-                    for (int j = (1 + i); j < 100; j++)
-                    {
-                        if (pw[i] == pw[j])
-                        {
-                            if ((pw[i+1] == pw[j+1]) && (pw[i+2] == pw[j+2]))stejnePodretezce = false;
-                        }
-                    }
+            for (int j = i + 1; j < COUNT_OF_ASCII; j++){
+                if (word[i] == '\0')
+                    break;
+                if (word[i] == word[j]){
+                    if (word[i+1] == word[j+1]) 
+                    notsamestrings = false;
                 }
-                break;
             }
-
-        case 4:
-            {
-                for (int i = 0; i < 100; i++)
-                {
-                    if (pw[i] == '\0')break;
-                    for (int j = (1 + i); j < 100; j++)
-                    {
-                        if (pw[i] == pw[j])
-                        {
-                            if ((pw[i+1] == pw[j+1]) && (pw[i+2] == pw[j+2]) && (pw[i+3] == pw[j+3]))stejnePodretezce = false;
-                        }
-                    }
-                }
-                break;
-            }
+        }
+        
     }
-    return stejnePodretezce;
-}
-int main(int argc, char const *argv[]){
-    int LEVEL = atoi(argv[1]);
-    int PARAM = atoi(argv[2]);
-    int nChars = 0;
-    int min = 101;
-    int currentLength = 0;
-    int max = 0;
-    char pw[101];
-    char chars[COUNT_OF_ASCII] = {0};
-    while(fgets(pw, 101, stdin) != NULL){
-        min = minimal(pw, min);
-        max = maximal(pw, max);
-        nChars+=wordNChars(pw, chars);
-
-        if(LEVEL == 1 && containsLowAndUpperCharacter(pw) == 1){
-            printf("%s", pw);
+    if(param == 3){
+        for (int i = 0; i < COUNT_OF_ASCII; i++){
+            if (word[i] == '\0'){
+                break;
+            }
+            for (int j = i + 1; j < COUNT_OF_ASCII; j++){
+                if (word[i] == '\0'){
+                break;
+            }
+                if (word[i] == word[j]){
+                    if ((word[i+1] == word[j+1]) && (word[i+2] == word[j+2]))
+                    notsamestrings = false;
+                }
+            }
         }
-        if(LEVEL == 2 && containsLowAndUpperCharacter(pw) == 1 && havingRightCharset(pw, PARAM) == 1){
-            printf("%s", pw);
-        }
-        if(LEVEL == 3 && containsLowAndUpperCharacter(pw) == 1 && havingRightCharset(pw, PARAM) == 1 && notContainsSameChars(pw, PARAM)){
-            printf("%s", pw);
-        }
-        if(LEVEL == 4 && containsLowAndUpperCharacter(pw) == 1 && havingRightCharset(pw, PARAM) == 1 && notContainsSameChars(pw, PARAM) && notContainsSameStrings(pw, PARAM)){
-            printf("%s", pw);
-        }
-
         
     }
 
-    printf("\nNCHAR: %d\nMinimalni delka: %d\nMaximalni delka: %d", nChars,min,max);
+    if(param == 4){
+        for (int i = 0; i < COUNT_OF_ASCII; i++){
+            if (word[i] == '\0'){
+                break;
+            }
+            for (int j = i + 1; j < COUNT_OF_ASCII; j++){
+                if (word[i] == '\0'){
+                break;
+            }
+                if (word[i] == word[j]){
+                    if ((word[i+1] == word[j+1]) && (word[i+2] == word[j+2]) && (word[i+3] == word[j+3]))
+                    notsamestrings = false;
+                }
+            }
+        }
+        
+    }
+
+    return notsamestrings;
+}
+
+
+
+int main(int argc, char const *argv[]){
+    int LEVEL = atoi(argv[1]);
+    int PARAM = atoi(argv[2]);
+    char statsChar[] = "--stats";
+    int nChars = 0;
+    int min = 101;
+    int currentLength = 0;
+    char countOfChars[101] = {0};
+    int n;
+    bool stats = true;
+    float wordCount = 0;
+    float allCount = 0;
+    float avg; 
+    char pw[101];
+    char chars[COUNT_OF_ASCII] = {0};
+
+    if(argc < 2 || argc > 4){
+        printf("Byl zadan nespravny pocet argumentu\n");
+        return 0;
+    }
+
+    if(LEVEL < 1 || LEVEL > 4){
+        printf("Byla zadana nespravna hodnota u argumentu LEVEL\n");
+        return 0;
+    }
+
+    if(PARAM < 1 || PARAM > 4){
+        printf("Byla zadana nespravna hodnota u argumentu PARAM\n");
+        return 0;
+    }
+
+    while(fgets(pw, 101, stdin) != NULL){
+        wordCount++;
+        min = minimal(pw, min);
+        
+        if(LEVEL == 1 && containsLowAndUpperCharacter(pw)){
+            printf("\n%s", pw);
+        }
+        if(LEVEL == 2 && containsLowAndUpperCharacter(pw) && havingRightCharset(pw, PARAM)){
+            printf("\n%s", pw);
+        }
+        if(LEVEL == 3 && containsLowAndUpperCharacter(pw) && havingRightCharset(pw, PARAM) && notContainsSameChars(pw, PARAM)){
+            printf("\n%s", pw);
+        }
+        if(LEVEL == 4 && containsLowAndUpperCharacter(pw) && havingRightCharset(pw, PARAM) && notContainsSameChars(pw, PARAM) && notContainsSameStrings(pw, PARAM)){
+            printf("\n%s", pw);
+        }
+        
+        int i = 0;
+        while(pw[i] != '\0'){
+            allCount++;
+            i++;
+        }
+        
+
+        for (int i = 0; i < COUNT_OF_ASCII; i++){
+            if (pw[i] == '\0') {
+                break;
+            }
+            for (int j = 0; j < COUNT_OF_ASCII; j++){
+                if (countOfChars[j] == '\0'){
+                    countOfChars[j] = pw[i];
+                    break;
+                }
+                if (pw[i] == countOfChars[j]){
+                    break;
+                }
+            }
+        }
+        
+    }
+
+    while(countOfChars[nChars] != '\0') {
+        nChars++; 
+    }
+
+    n = 0; 
+    while(statsChar[n]!='\0'){
+        if(argv[3][n]!=statsChar[n])
+            stats=false;
+            
+        n++;        
+    }
+
+
+    if(stats){
+        avg = allCount/wordCount;
+        printf("\n\n     STATISTIKA     \n--------------------\nNCHAR: %d\nMinimalni delka: %d\nPrumerna delka: %f\n--------------------\n", nChars,min,avg);
+    }
+    
     
 
 
